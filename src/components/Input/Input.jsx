5684cb66f53f831e452c105d6ce82a19476626a7
Sedana Yoga <9g.yoga27@gmail.com>
2022-07-16 15:05:02 +0800

    add: single input, textarea, and file upload

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,33 +1,73 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import styles from './Input.module.scss'
 
 const Input = ({ children, type, onChange, required, name }) => {
   const [value, setValue] = useState('')
   const [isFocus, setIsFocus] = useState(false)
+  const hiddenFileInput = useRef(null)
 
   const changeHandler = (e) => {
-    const { value, name } = e.target
-    setValue(value)
-    if (onChange) onChange({ value, name })
-    if (value && value !== '') {
-      setIsFocus(true)
+    if (type === 'file') {
+      const value = e.target.files[0]
+      const name = e.target.name
+      if (onChange) onChange({ value, name })
     } else {
-      setIsFocus(false)
+      const { value, name } = e.target
+      setValue(value)
+      if (onChange) onChange({ value, name })
+      if (value && value !== '') {
+        setIsFocus(true)
+      } else {
+        setIsFocus(false)
+      }
     }
+  }
+
+  const fileClickHandle = () => {
+    hiddenFileInput.current.click()
   }
 
   return (
     <div className={styles.input}>
-      <input
-        type={type}
-        onChange={changeHandler}
-        value={value}
-        name={name}
-        required={required}
-      />
-      <label htmlFor={name} className={isFocus ? styles.active : ''}>
-        {children}
-      </label>
+      {type === 'textarea' ? (
+        <textarea
+          onChange={changeHandler}
+          value={value}
+          name={name}
+          required={required}
+          className={styles.textarea}
+        ></textarea>
+      ) : type === 'file' ? (
+        <input
+          type='file'
+          onChange={changeHandler}
+          accept='/image/*'
+          name={name}
+          required={required}
+          style={{ display: 'none' }}
+          ref={hiddenFileInput}
+        />
+      ) : (
+        <input
+          type={type}
+          onChange={changeHandler}
+          value={value}
+          name={name}
+          required={required}
+        />
+      )}
+
+      {type !== 'file' && (
+        <label htmlFor={name} className={isFocus ? styles.active : ''}>
+          {children}
+        </label>
+      )}
+
+      {type === 'file' && (
+        <button type='button' onClick={fileClickHandle}>
+          {children}
+        </button>
+      )}
     </div>
   )
 }
--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+import Image from 'next/image'
 import NormalLayout from '../../layouts/Normal/NormalLayout'
 import Input from '../../components/Input/Input'
 import styles from '../../styles/pages/Admin.module.scss'
@@ -11,19 +13,35 @@ const Admin = () => {
   }
 
   return (
-    <div className={styles.admin}>
-      <h1 className={styles.adminTitle}>Works Form</h1>
-      <form className={styles.adminForm}>
-        <Input onChange={changeHandler} name='title'>
-          Title
-        </Input>
-        <Input onChange={changeHandler} name='quote'>
-          Quote
-        </Input>
-        <Input onChange={changeHandler} name='brief'>
-          Brief
-        </Input>
-      </form>
+    <div>
+      <Head>
+        <title>Admin Page</title>
+      </Head>
+      <div className={styles.admin}>
+        <h1 className={styles.adminTitle}>Works Form</h1>
+        <form className={styles.adminForm}>
+          <Input onChange={changeHandler} name='title'>
+            Title
+          </Input>
+          <Input onChange={changeHandler} name='quote'>
+            Quote
+          </Input>
+          <Input onChange={changeHandler} name='brief' type='textarea'>
+            Brief
+          </Input>
+          <Input onChange={changeHandler} name='logo' type='file'>
+            Click me to upload a logo
+          </Input>
+          {values.logo && (
+            <Image
+              alt='Logo Image'
+              src={URL.createObjectURL(values.logo)}
+              width='100px'
+              height='100px'
+            />
+          )}
+        </form>
+      </div>
     </div>
   )
 }
